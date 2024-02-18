6bdbdf5217293e76e6f3eb0b0f2e4de4e905b3c6
Preetish Biswal <78357366+Preetishx77@users.noreply.github.com>
2024-02-18 19:18:41 +0530

    Update ngoRoute.ts

--- routes/ngoRoute.ts
+++ routes/ngoRoute.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/signup", NGOController.signUp);
 router.get("/signup", NGOController.getNgoData);
+router.post("/user", NGOController.getNgoData);
 router.post("/campaign/create", NGOController.campaignCreate);
 
 export { router as ngoRouter };
