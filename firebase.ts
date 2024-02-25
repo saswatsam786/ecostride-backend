875712c5f7c3802af9ecb2380b23eb750977bdcd
saswatsam786 <saswat.sam786@gmail.com>
2024-02-26 00:20:01 +0530

    DOne

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAdVG57t2f9jXkL5yBfAziUIgOSb9tA1D8",
-    authDomain: "ecostride-333d8.firebaseapp.com",
-    projectId: "ecostride-333d8",
-    storageBucket: "ecostride-333d8.appspot.com",
-    messagingSenderId: "446449876620",
-    appId: "1:446449876620:web:a2080fd235ef451ba205a9",
-    measurementId: "G-WJ954HBP94"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 // Initialize Firebase
@@ -18,4 +18,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 // const analytics = getAnalytics(app);
 
-export { db }
+export { db };
